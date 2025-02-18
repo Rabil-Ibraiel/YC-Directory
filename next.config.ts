51931e7a80146f6463cd5f6a4300bf75989e007f
Rabil <96547109+Rabil-Ibraiel@users.noreply.github.com>
2025-02-18 06:19:01 +0300

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  transpilePackages: ["lucide-react"],
+  experimental: {
+    esmExternals: true,
+  },
   typescript: {
     ignoreBuildErrors: true
   },
